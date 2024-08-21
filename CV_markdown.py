31c7391e65999f2d3d5b00a8cc3508cdacd1c0e2
andresfari <andresfariasf2@gmail.com>
2024-08-20 18:08:54 -0600

    Add files via upload

---
+++ CV_markdown.py
@@ -0,0 +1,67 @@
+import streamlit as st
+
+st.markdown( "# ANDRÉS FARIAS FIGUEROA")
+
+col6,col7,col8 = st.columns(3)
+with col6:
+    st.markdown(" ")
+
+with col7:
+    st.image("C:/fotoCV.jpg",width=300)
+    
+with col8:
+    st.markdown(" ")
+
+
+col1, col2,col3 = st.columns(3)
+with col1:
+    st.markdown("**CELULAR** ")
+    st.markdown("*+52 (443) 3689256*")
+
+with col2:
+    st.markdown("**CORREO ELECTRÓNICO**")
+    st.markdown(" *andresfariasf@hotmail.com* ")
+
+with col3:
+    st.markdown("**LINKEDIN**")
+    st.markdown("*www.linkedin.com/in/andres-farias-figueroa-711a0b2b1*")
+    
+st.markdown("*Soy un estudiante responsable, en busca de experiencia laboral que fortalezca mi formación personal.*")
+st.markdown("---------------------------------------------")
+st.markdown(" ## EDUCACIÓN ")
+st.markdown(" ### UNIVERSIDAD PANAMERICANA GDL  |   Administración y finanzas |    agosto 2021 - diciembre 2025")
+st.markdown("---------------------------------------------")
+st.markdown(" ## EXPERIENCIA LABORAL")
+st.markdown(" ### JAFER DISTRIBUCIÓN | Auxiliar de compras | Junio 2022 - Diciembre 2022")
+st.markdown("Identificar y proporcionar material para la operación del negocio. Contactar proveedores y acordar el mejor precio y forma de distribución para la empresa. Mantener un registro diario de entradas y salidas del inventario y señalar los recursos empleados. Identificar compras y ventas de registros anteriores, y reorganizarlos en un nuevo registro contable. ")
+st.markdown("---------------------------------------------")
+st.markdown("## EXTRA -CURRICULAR")
+st.markdown("### BREED | Planeación Financiera | Agost0 2023 - Diciembre 2023")
+st.markdown("Solución de problemas sobre un proyecto existente de recolección de donativos para caridad. Crear un plan para captar recursos y cubrir el costo del proyecto. ")
+st.markdown("### Prepa TEC | Departamento de admisiones | Agost0 2018 - Junio 2021")
+st.markdown("Trabajo administrativo en el área de admisiones. Organización de eventos académicos. ")
+st.markdown("### Proyectos académicos/administrativos")
+st.markdown("- Base de datos B-Sensible (2024)")
+st.markdown("- Análisis corporativo “Schnitzer” (2023)")
+st.markdown("- Plan de emprendimiento “TEQUILA AL MAL” (2023)")
+st.markdown("---------------------------------------------")
+
+col4,col5 = st.columns(2)
+
+with col4:
+    st.markdown("## HABILIDADES")
+    st.markdown("- Herramientas tecnológicas: Paquetería Office, Canva, Insignia, Contpaq, SAP, Python, Streamlit ")
+    st.markdown(" - Finanzas bursátiles ")
+    st.markdown("- Contabilidad ")
+    st.markdown("- Excel Avanzado ")
+    st.markdown(" - Resolución de problemas")
+    st.markdown(" - Pensamiento crítico")
+    st.markdown(" - Trabajo en equipo")
+with col5:
+    st.markdown("## IDIOMAS")
+    st.markdown(" - **Español** Nativo")
+    st.markdown(" - **Inglés** Avanzado")
+    st.markdown(" - **Alemán** Básico")
+   
+    
+    
